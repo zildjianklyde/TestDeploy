c2207f420ebae1601427b2b7f9bdac540c8baa17
zildjianklyde <zildjianklyde@gmail.com>
2024-06-12 00:52:22 +0800

    new update

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const app = express()
 
 app.use(express.static('assets'))
 
-app.use('/', (req,res)=>{ 
-    res.sendFile(path.join(__dirname,'index.html'))
+
+app.use('/klyde', (req, res) => {
+    res.send("Hello I'm Klyde")
 })
 
-app.use('/klyde',(req,res)=>{
-    res.send("Hello I'm Klyde")
-} )
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+
 
 const PORT = process.env.PORT || 9090
-app.listen(PORT, ()=> console.log(`Listening to PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`))
 
